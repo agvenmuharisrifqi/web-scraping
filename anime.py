b46f7cabd95a019af2b6e4a94368566a1587dee6
agvenmuharisrifqi <agven25@gmail.com>
2022-08-18 23:53:26 +0700

    Try to scrap list anime

---
+++ anime.py
@@ -0,0 +1,53 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from bs4 import BeautifulSoup
+from time import sleep
+from create_excel import CreateExcel
+
+def scrolling_page(browser):
+    pass
+
+def main():
+    options = webdriver.ChromeOptions()
+    options.add_argument('--ignore-certificate-errors')
+    options.add_argument('--incognito')
+    options.add_argument('--no-sandbox')
+    options.add_argument('--headless')
+    options.add_experimental_option('excludeSwitches', ['enable-automation'])
+    browser = webdriver.Chrome('chromedriver_linux64/chromedriver', chrome_options=options)
+
+    data = []
+    for i in range(1, 3):
+        browser.get('https://animasu.club/pencarian/?urutan=abjad&halaman=%s' % (str(i)))
+        sleep(2)
+        height = 0
+        while True:
+            sleep(0.5)
+            browser.execute_script('window.scrollTo(0, ' + str(height) + ');')
+            window_height = browser.execute_script('return document.body.scrollHeight')
+            height += 300
+            if height > window_height:
+                soup = BeautifulSoup(browser.page_source, 'lxml')
+                items = soup.find_all('div', attrs={'class': 'bs'})
+                item = [
+                    (
+                        item.find('div', attrs={'class': 'tt'}).text,
+                        item.find('div', attrs={'class': 'typez'}).text,
+                        item.find('span', attrs={'class': 'sb'}).text
+                    ) for item in items
+                ]
+                data += item
+                break
+    
+    header = ['Judul', 'Type', 'Status']
+    excel = CreateExcel(filename='anime.xlsx', header=header)
+    excel.add_data(data)
+    excel.close()
+
+    sleep(2)
+    browser.close()
+
+
+if __name__ == '__main__':
+    main()
---
+++ create_excel.py
@@ -0,0 +1,28 @@
+import xlsxwriter
+
+class CreateExcel:
+
+    def __init__(self, filename, header):
+        self.filename = filename
+        self.workbook = xlsxwriter.Workbook(self.filename)
+        self.worksheet = self.workbook.add_worksheet(self.filename)
+        self.header = header
+        self.style = {
+            'header': {'font_name': 'Roboto', 'font_size': 11, 'bold': True, 'align': 'center', 'valign': 'vcenter', 'bg_color': 'green', 'font_color': '#FFFFFF', 'border': 1},
+            'content': {'font_name': 'Roboto', 'font_size': 11, 'align': 'center', 'valign': 'vcenter', 'border': 1}
+        }
+        self.add_header()
+
+    def add_header(self):
+        for col, value in enumerate(self.header):
+            self.worksheet.write(0, col, value, self.workbook.add_format(self.style['header']))
+
+    def add_data(self, data):
+        row = 1
+        for item in data:
+            for col, value in enumerate(item):
+                self.worksheet.write(row, col, value, self.workbook.add_format(self.style['content']))
+            row += 1
+
+    def close(self):
+        self.workbook.close()
